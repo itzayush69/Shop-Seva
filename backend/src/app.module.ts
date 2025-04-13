498156b22dc258881e9916f9b07f7a9c372891aa
itzayush69 <itzayushh18@gmail.com>
2025-04-13 13:30:34 +0530

    feat: Add Shop and Product modules to the application

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ShopModule } from './shop/shop.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ShopModule, ProductModule],
 })
 export class AppModule {}
